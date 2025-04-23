46095f5e2c3fc6e89650daa8b048fd69b92df885
TotalDarkness <totaldarkness@totaldarkness.net>
2025-04-23 15:46:04 -0400

    add wire connector component

--- src/view/Circuit.rs
+++ src/view/Circuit.rs
@@ -1,17 +1,17 @@
 use leptos::*;
 use leptos_meta::Title;
-use web_sys::DragEvent;
+use web_sys::{DragEvent, MouseEvent};
 use crate::model::{gate_type::GateType, logic_gate::LogicGate};
 use crate::view::LogicGates::{RenderGate, RotatableGate};
 
 #[component]
 pub fn render() -> impl IntoView {
-    let dropped_gates = create_rw_signal::<Vec<LogicGate>>(vec![]);
     view! {
         <Title text="Logic Gates"/>
         <div style="user-select: none; display: flex; height: 100vh; font-family: sans-serif;" >
             <DragZone />
-            <DropZone dropped_gates=dropped_gates />
+            <DropZone />
+            <CircuitConnector />  
         </div>
     }
 }
@@ -32,7 +32,8 @@ fn DragZone() -> impl IntoView {
 }
 
 #[component]
-fn DropZone(dropped_gates: RwSignal<Vec<LogicGate>>) -> impl IntoView {
+fn DropZone() -> impl IntoView {
+    let dropped_gates = create_rw_signal::<Vec<LogicGate>>(vec![]);
     let handle_drop = move |e: DragEvent| {
         e.prevent_default();
         let x = e.offset_x();
@@ -65,5 +66,52 @@ fn DropZone(dropped_gates: RwSignal<Vec<LogicGate>>) -> impl IntoView {
                 />
             </Show>
         </div>
+    }
+}
+
+#[component]
+pub fn CircuitConnector() -> impl IntoView {
+    let (start_point, set_start_point) = create_signal(None::<(f64, f64)>);
+    let (hover_point, set_hover_point) = create_signal(None::<(f64, f64)>);
+    let (connections, set_connections) = create_signal(Vec::new());
+
+    let on_mouse_move = move |ev: MouseEvent| {
+        set_hover_point.set(Some((ev.offset_x() as f64, ev.offset_y() as f64)));
+    };
+
+    let on_click = move |ev: MouseEvent| {
+            let point = (ev.offset_x() as f64, ev.offset_y() as f64);
+            match start_point.get() {
+                Some(start) => {
+                    set_connections.update(|conns| conns.push((start, point)));
+                    set_start_point.set(None);
+                    set_hover_point.set(None);
+                },
+                None => set_start_point.set(Some(point)),
+            }
+        };
+
+    view! {
+        <svg
+            style="border: 1px solid gray;"
+            on:mousemove=on_mouse_move
+            on:click=on_click
+        >
+            {move || connections.get().into_iter().map(|((x1, y1), (x2, y2))| {
+                view! {
+                    <line x1=x1 y1=y1 x2=x2 y2=y2 stroke="black" stroke-width="2"/>
+                }
+            }).collect::<Vec<_>>()}
+
+            {move || {
+                if let (Some((x1, y1)), Some((x2, y2))) = (start_point.get(), hover_point.get()) {
+                    Some(view! {
+                        <line x1=x1 y1=y1 x2=x2 y2=y2 stroke="red" stroke-dasharray="4"/>
+                    })
+                } else {
+                   None
+                }
+            }}
+        </svg>
     }
 }
