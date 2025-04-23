94ca622404397b53a21b425273984583cacf8d75
TotalDarkness <totaldarkness@totaldarkness.net>
2025-04-22 21:08:06 -0400

    add view module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod app;
+pub mod view;
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
---
+++ src/view/mod.rs
@@ -0,0 +1,6 @@
+#[allow(non_snake_case)]
+pub(crate) mod HomePage;
+#[allow(non_snake_case)]
+pub(crate) mod LogicGates;
+#[allow(non_snake_case)]
+pub(crate) mod NotFound;
